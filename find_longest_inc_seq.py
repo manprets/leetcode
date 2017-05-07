fc0c01ad899674ec592e50cfed0aca088a994b3c
Manpreet Singh <manprets@hotmail.com>
2017-05-06 22:47:11 -0400

    feat: printing length at each index

--- find_longest_inc_seq.py
+++ find_longest_inc_seq.py
@@ -5,24 +5,26 @@ mylist = [10, 9, 2, 5, 3, 7, 101, 18, 19]
 def find_LIS(ip_list = mylist):
     print('mylist: ', mylist)
     curr = None
-    length = 0
-    for idx in range(len(mylist)):
+    lenlist = len(mylist)
+    
+    for idx in range(lenlist):
         seq = []
         start = mylist[idx]
         curr = start
         prev = None
         seq.append(curr)
         # print(curr)
-        length += 1
+        length = 1
         for jdx in range(idx+1, len(mylist)):
             next = mylist[jdx]
             if curr<next:
                 seq.append(next)
                 prev = curr
                 curr = next
+                length += 1
             else:
                 pass
-        print('start: {}, seq: {}'.format(start,seq))
+        print('start: {}, seq: {}, length: {}'.format(start,seq,length))
 
     
 
