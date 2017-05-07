bdc3fa006dc782742ebb9d344c1a23c9f98fc877
Manpreet Singh <manprets@hotmail.com>
2017-05-06 22:39:53 -0400

    feat: first try

--- find_longest_inc_seq.py
+++ find_longest_inc_seq.py
@@ -1,10 +1,38 @@
 # python code for Longest Increasing Sequence
 
-mylist = [10, 9, 2, 5, 3, 7, 101, 18]
+mylist = [10, 9, 2, 5, 3, 7, 101, 18, 19]
 
 def find_LIS(ip_list = mylist):
-    for num in mylist:
-       print(num)
+    print('mylist: ', mylist)
+    curr = None
+    length = 0
+    for idx in range(len(mylist)):
+        seq = []
+        start = mylist[idx]
+        curr = start
+        prev = None
+        seq.append(curr)
+        # print(curr)
+        length += 1
+        for jdx in range(idx+1, len(mylist)):
+            next = mylist[jdx]
+            if curr<next:
+                seq.append(next)
+                prev = curr
+                curr = next
+            else:
+                pass
+        print('start: {}, seq: {}'.format(start,seq))
+
+    
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     find_LIS()
